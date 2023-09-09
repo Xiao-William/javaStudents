beef87071e7c04276d2c56a4d992a1b149eeb1bd
肖迪荣 <2211587076@qq.com>
2023-09-09 17:38:22 +0800

    udpatge

--- src/OOP/oopProps.java
+++ src/OOP/oopProps.java
@@ -2,12 +2,18 @@ package OOP;
 
 public class oopProps {
     public static void main(String[] args) {
-        // TODO: 2023/9/9  1.声明类
-        // TODO: 2023/9/9  2.创建对象的语法
+        // TODO: 2023/9/9 1.声明类
+        // TODO: 2023/9/9 2.创建对象的语法
         Cooking cooking = new Cooking();
         cooking.name = "红烧排骨";
         cooking.execute();
-
+        Animal a1 = new Animal();
+        a1.name = "猫";
+        System.out.println(a1.type = "在吃饭");
+        a1.run();
+        Students study = new Students();
+        study.name = "肖迪荣";
+        study.Study();
     }
 }
 
@@ -16,10 +22,28 @@ class Cooking {
     String type = "红烧";
     String relish = "大料";
 
-//    执行
-    void  execute(){
+    // 执行
+    void execute() {
         System.out.println(this.name);
         System.out.println(this.type);
+        System.out.println(this.type);
         System.out.println(this.relish);
+    }
+}
+
+class Animal {
+    String name;
+    String type;
+
+    void run() {
+        System.out.println("pao" + name);
+    }
+}
+
+class Students {
+    String name;
+
+    void Study() {
+        System.out.println("在听课" + name);
     }
 }
