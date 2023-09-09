d2c2c7998964345ebe86373a2d6952ffbf081e6b
肖迪荣 <2211587076@qq.com>
2023-09-09 17:39:30 +0800

    update

--- src/OOP/oopProps.java
+++ src/OOP/oopProps.java
@@ -2,8 +2,10 @@ package OOP;
 
 public class oopProps {
     public static void main(String[] args) {
+
         // TODO: 2023/9/9 1.声明类
         // TODO: 2023/9/9 2.创建对象的语法
+
         Cooking cooking = new Cooking();
         cooking.name = "红烧排骨";
         cooking.execute();
